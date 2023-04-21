13a6457eaaf4ce6da3beba3d0eff92afcbbd21f5
kakekita <117062289+kakekita@users.noreply.github.com>
2023-04-21 11:08:48 +0900

    Add files via upload

--- mySketch.js
+++ mySketch.js
@@ -6,13 +6,13 @@ var typeStatus = {
   count1: 0,
   count2: null,
   pat: null,
-  tmp: "",
-  hist: "",
+  tmp: '',
+  hist: '',
 };
 
 function getData() {
   var api_url =
-    "https://script.google.com/macros/s/AKfycbyZe0sLk9whSFhUBcC5dRCuxVovLYkCVVXLHUACYQ8SqqLNj_BMdcbdRD6ROtTxaiEE/exec"; //生成したAPIのURLを指定
+    'https://script.google.com/macros/s/AKfycbyZe0sLk9whSFhUBcC5dRCuxVovLYkCVVXLHUACYQ8SqqLNj_BMdcbdRD6ROtTxaiEE/exec'; //生成したAPIのURLを指定
   fetch(api_url)
     .then(function (fetch_data) {
       return fetch_data.json();
@@ -20,21 +20,21 @@ function getData() {
     .then(function (json) {
       sents = json;
       console.log(sents);
-      console.log("取得完了");
-      fetch("./roms.json")
-        .then((response) => response.json())
+      console.log('取得完了');
+      fetch('./roms.json')
+        .then(response => response.json())
         .then(function (data) {
           roms = data;
           console.log(roms);
-          getRomSentLists("にほんかいにじゃっぱーん");
-          document.addEventListener("keydown", keydown_e);
+          getRomSentLists('にほんかいにじゃっぱーん');
+          document.addEventListener('keydown', keydown_e);
         });
     });
 }
 
 function getRomSentLists(hiragana) {
   hiragana = String(hiragana);
-  var h_sp = hiragana.split("");
+  var h_sp = hiragana.split('');
   var s_rom = Array(h_sp.length);
   var r_keys = Object.keys(roms);
   console.log(r_keys);
@@ -43,6 +43,17 @@ function getRomSentLists(hiragana) {
       s_rom[h] = roms[h_sp[h]];
     } else {
       s_rom[h] = h_sp[h];
+    }
+  }
+
+  for (var s = 0; s < s_rom.length - 1; s++) {
+    var ch = String(h_sp[s]) + String(h_sp[s + 1]);
+    switch(ch){
+      case 'じゃ':
+      case 'じゅ':
+      case 'じょ':
+        s_rom[s].push("j"+s_rom[s+1][0].replace(/xy|ly/,""))
+        break;
     }
   }
 
@@ -56,12 +67,18 @@ function getRomSentLists(hiragana) {
   typeStatus.count1 = 0;
 }
 
+function getNextKey() {
+  for (var c in word_r[typeStatus.count1]) {
+
+  }
+}
+
 function keydown_e(e) {
   var key = e.key;
   for (var c in word_r[typeStatus.count1]) {
     if (
       key ===
-        word_r[typeStatus.count1][c].split("")[
+        word_r[typeStatus.count1][c].split('')[
           typeStatus.count2[typeStatus.count1]
         ] &&
       word_r[typeStatus.count1][c].indexOf(typeStatus.tmp) != -1
@@ -69,12 +86,12 @@ function keydown_e(e) {
       typeStatus.pat[typeStatus.count1] = c;
       if (
         typeStatus.count2[typeStatus.count1] >=
-        word_r[typeStatus.count1][c].split("").length - 1
+        word_r[typeStatus.count1][c].split('').length - 1
       ) {
         //typeStatus.count2[typeStatus.count1]++;
         typeStatus.count1++;
         typeStatus.count2[typeStatus.count1] = 0;
-        typeStatus.tmp = "";
+        typeStatus.tmp = '';
       } else {
         typeStatus.count2[typeStatus.count1]++;
         typeStatus.tmp += key;
@@ -90,9 +107,9 @@ function keydown_e(e) {
 }
 
 function repaint() {
-  var all_t = "";
-  var red_t = "";
-  var black_t = "";
+  var all_t = '';
+  var red_t = '';
+  var black_t = '';
 
   for (var c in word_r) {
     /*if (c <= typeStatus.count1) {
@@ -107,14 +124,14 @@ function repaint() {
         typeStatus.count2[c]
       );
     }*/
-    all_t += word_r[c][typeStatus.pat[c]]
+    all_t += word_r[c][typeStatus.pat[c]];
   }
-  
-  black_t = all_t.replace(typeStatus.hist,"")
+
+  black_t = all_t.replace(typeStatus.hist, '');
   red_t = typeStatus.hist;
-  
-  document.getElementById("red-text").textContent = red_t;
-  document.getElementById("black-text").textContent = black_t;
+
+  document.getElementById('red-text').textContent = red_t;
+  document.getElementById('black-text').textContent = black_t;
 }
 
 getData();
